da10cb066f6acf9199000c11128a1290c3905471
Dean Conway <dean.conway@delv.com>
2022-10-03 09:42:06 +1100

    -upd: replaced public key

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ sudo apt-get -y upgrade
 sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano
 mkdir ~/.ssh; chmod 0700 ~/.ssh
 cat << EOF > ~/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5VJBLZt7QR01HkLhTVTjSjve/6vNWJHwI3yxMI5Q3TGiuEVINMJiP0sp6cr8xRe7Ix24a1ZAc3fdu0z658JXKN rsa-key-20190820
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDaB/T+TpF/4IrwpGWp+ob/hZT4kWaMMIaJI2NuqtwdRh8+Rhxy8TR5Vb4NSwZacyNCWk01JjL4hkqycPxUqISSD8+vTn0MfKfGHThklvsH2C1pbaoc9Zj2W8Ol0P4aQ/nw6bo+jikw+22eSA/vduG5Ok5fogApjJ4cqlIKs+4OV1B3RXiw4czh9mRH7kpsCkVc7+HgdkhUkO32NLYY+hYIAp5/AVbGqEprbcp88z04QnTcRC41P7cwfCIUPWZPKdsgD8q7IeR7gFnqDXOUyYgHOncgu71+VecHKyZIhRfm2WwtoMh7Y2nrlvFS4qG2OYS7F6S6sskijZb+/fzXyAccB3DL8/oURpszxygRxGgC4H2OSLhGezjsa3qoZ3uis6HC0IUdiwaLXIyMetvMiDvzlIyuNU+60AzNjRqVqogavKVMWxZYD55KiCl7woRKQJrP550MTmaVxUd+6wpTrPViDStlmOYQlVELv5LwDr+A1uZqLDf5DcC7mqZ1N6RqK3s=
 EOF
 chmod 0600 ~/.ssh/authorized_keys
 mkdir ~/ccminer
 cd ~/ccminer
 wget https://github.com/Oink70/Android-Mining/releases/download/v0.0.0-1/ccminer
-wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json
+wget https://github.com/conwaydean/android-mining/blob/05b838262712d4558b8d5d99e606c01246943205/config.json
 chmod +x ccminer
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
